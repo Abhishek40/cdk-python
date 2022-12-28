2c8d93fd3ee0102c7e9eb30e91be669bee373f8a
Abhishek Pal <abhishekraj144@gmail.com>
2022-12-28 08:25:10 +0530

    initial commit

--- static_website/static_website_stack.py
+++ static_website/static_website_stack.py
@@ -1,11 +1,7 @@
 from constructs import Construct
 from aws_cdk import (
-    Duration,
     Stack,
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
+    aws_s3 as _s3,
 )
 
 
@@ -13,14 +9,3 @@ class StaticWebsiteStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "StaticWebsiteQueue",
-            visibility_timeout=Duration.seconds(300),
-        )
-
-        topic = sns.Topic(
-            self, "StaticWebsiteTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
